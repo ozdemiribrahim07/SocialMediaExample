cda1b529792c0b5c22ef461678f6e3a5de8a5bcc
ozdmirr <ozdmirr77@gmail.com>
2024-01-08 13:27:12 +0300

    log4net logging

---
+++ Core/CrossCuttingC/Caching/Microsoft/MemoryCacheManager.cs
@@ -0,0 +1,64 @@
+﻿
+using Core.Utilities.IoC;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingC.Caching.Microsoft
+{
+    public class MemoryCacheManager : ICacheManager
+    {
+        private IMemoryCache _memoryCache;
+        private readonly List<string> _cacheKeys;
+
+        public MemoryCacheManager()
+        {
+            _memoryCache = ServiceTool.serviceProvider.GetService<IMemoryCache>();
+            _cacheKeys = new List<string>();
+        }
+
+
+        public void Add(string key, object value, int duration)
+        {
+            _memoryCache.Set(key,value,TimeSpan.FromMinutes(duration));
+        }
+
+        public T Get<T>(string key)
+        {
+            return _memoryCache.Get<T>(key);
+        }
+
+        public object Get(string key)
+        {
+            return _memoryCache.Get(key);
+        }
+
+        public bool IsAdd(string key)
+        {
+            return _memoryCache.TryGetValue(key, out _);
+        }
+
+        public void Remove(string key)
+        {
+            _memoryCache.Remove(key);
+            _cacheKeys.Remove(key);
+        }
+
+        public void RemoveByPattern(string pattern)
+        {
+            var regex = new Regex(pattern, RegexOptions.IgnoreCase | RegexOptions.Singleline | RegexOptions.Compiled);
+            var keysToRemove = _cacheKeys.Where(key => regex.IsMatch(key)).ToList();
+
+            foreach (var key in keysToRemove)
+            {
+                _memoryCache.Remove(key);
+                _cacheKeys.Remove(key);
+            }
+        }
+    }
+}
---
+++ Core/Utilities/LogMessages/AspectMessages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.LogMessages
+{
+    public static class AspectMessages
+    {
+        public static string WrongValidationType = "Yanlış Doğrulama Tipi";
+        public static string WrongLoggerType = "Yanlış Loglama Tipi";
+    }
+}
